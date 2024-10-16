2c671a569c4d659595cd28631e1d287116b61100
WrongAnswer90 <wangyice1537@163.com>
2024-10-16 13:50:53 +0800

    Updated on 2024-10-16 13:50

--- commiter.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "Updated on 2024-10-09 18:13"
-git push -f origin main
---
+++ update.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<windows.h>
+#include<wchar.h>
+using namespace std;
+inline void commit()
+{
+	char s[210];
+	time_t tmp;
+	time(&tmp);
+	struct tm *p;
+	p=localtime(&tmp);
+	sprintf(s,"git commit -m \"Updated on %d-%02d-%02d %02d:%02d\"",1900+p->tm_year,
+	1+p->tm_mon,p->tm_mday,p->tm_hour,p->tm_min);
+	
+	system("git add .");
+	system(s);
+	int z;
+	again:
+	z=system("git push -f origin main");
+	if(z){Sleep(20000);goto again;}
+}
+int main()
+{
+	commit();
+}
